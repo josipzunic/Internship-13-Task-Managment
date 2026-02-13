fa7fd8634865e6af6d08a83a5a056b428b675231
miabarada <mia.barada2005@gmail.com>
2026-02-13 18:23:10 +0100

    fix modal layout

--- frontend/src/components/taskModal.js
+++ frontend/src/components/taskModal.js
@@ -5,34 +5,34 @@ export function openTaskModal({ status }) {
       const modalContent = document.querySelector(".modal-content");
 
       modalContent.innerHTML = `
-      <div class="modul-header">
-         <h2>Create Task</h2>
-         <button class="exit-button"></button>
+      <div class="modal-header">
+         <h2 class="modal-title">Create Task</h2>
+         <button class="exit-button"><img src="./src/assets/exit.svg" alt="exit" class="icon"></button>
       </div>
         
       <form class="task-form">
-         <h3>Title:</h3>
+         <h3 class="input-title">Title:</h3>
          <input type="text" name="title" class="input-field" placeholder="Title" required>
-         <h3>Description:</h3>
+         <h3 class="input-title">Description:</h3>
          <textarea name="description" class="input-field"></textarea>
-         <h3>Start date:</h3>
+         <h3 class="input-title">Start date:</h3>
          <input type="date" name="startDate" class="input-field">            <h3>End date:</h3>
          <input type="date" name="endDate" class="input-field">
-         <h3>Estimate hours:</h3>
+         <h3 class="input-title">Estimate hours:</h3>
          <input type="number" name="estimateHours" min="0" class="input-field">
-         <h3>Priority:</h3>
+         <h3 class="input-title">Priority:</h3>
          <select name="priority" class="input-field">
             <option value="LOW">Low</option>
             <option value="MID">Middle</option>
             <option value="HIGH">High</option>
          </select>
-         <h3>Type:</h3>
+         <h3 class="input-title">Type:</h3>
          <select name="type" class="input-field">
             <option value="FEATURE">Feature</option>
             <option value="BUGFIX">Bugfix</option>
             <option value="IMPROVEMENT">Improvement</option>
          </select>
-         <h3>Asignee:</h3>
+         <h3 class="input-title" >Asignee:</h3>
          <input type="text" name="assignee" placeholder="Asignee" class="input-field">
             
          <div class="button-options">
