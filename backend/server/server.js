f994413b155c250fd5c778ab39b9da8e1a727b04
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-02-14 18:06:42 +0100

    Add CRUD requests, fix timestamp validation

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,10 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import { database, initDatabase } from "../database/database.js";
+import {
+  getTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} from "./controllers/taskController.js";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/api/tasks", getTasks);
+app.get("/api/tasks/:id", getTask);
+app.post("/api/tasks", createTask);
+app.patch("/api/tasks/:id", updateTask);
+app.delete("/api/tasks/:id", deleteTask);
 
 const startServer = async () => {
   await database.query("SELECT 1");
